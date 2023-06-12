817cb0697b514355730699024beb74a305495abe
asadkarim <asadkarim33@gmail.com>
2023-06-12 19:23:15 +0500

    icon change

--- React/my-todo/src/todolist/TodoItem.jsx
+++ React/my-todo/src/todolist/TodoItem.jsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import DeleteIcon from "../assets/deleteicon.png"
 import "./todo.css"
+import { FaHeart } from 'react-icons/fa';
+
 
 export const TodoItem = ({title, deleteTodo}) =>{
   const [completed, setCompleted] = useState(false);
@@ -14,8 +16,8 @@ export const TodoItem = ({title, deleteTodo}) =>{
     <div className="todo-item" id="todo-1">
       <p className="todo-task-title" style={{ textDecoration: completed ? "line-through" : "none" }}>{title}</p> 
       <div className="img-but">      
-      <img className="todo-icon" src={DeleteIcon} onClick={deleteTodo} />
-      <button onClick={OnPress}   style={{ borderColor: completed ? "green" : "black"}} > A</button>
+      <img className="todo-icon" src={DeleteIcon} style={{borderColor: completed ? "red" : "white" }} onClick={deleteTodo} />
+      <FaHeart onClick={OnPress}  style={{ color: completed ? "red" : "black"}} />
       </div>
     </div>
   )
