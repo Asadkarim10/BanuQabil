72a83269f912a5b2e46666fa46e7dd11a2ef7426
asadkarim <asadkarim33@gmail.com>
2023-06-15 21:35:00 +0500

    user interfrence change

--- React/calculator/src/Calculator/calculator.jsx
+++ React/calculator/src/Calculator/calculator.jsx
@@ -2,8 +2,6 @@ import React ,{useState, usestate } from 'react';
 import './calc.css'
 
 
-
-
 export const Calculator = ()=> {
 
 const [ result, setResult]=useState('');
@@ -22,28 +20,35 @@ const calculate=()=> {
 
 return (
 
-<div className='calc'>
 
-    <input type="text" placeholder='0' id='answer' value={result} />
+
+<div className='wrapper'>
+<div className='line'>
+</div>
+    <div className='calc' >
+    <input className='Inputt' type="text" placeholder='0' id='answer' value={result} />
+
     <input type="button" value='9' className='button' onClick={clickHandler}/>
     <input type="button" value='8' className='button' onClick={clickHandler}/>
     <input type="button" value='7' className='button' onClick={clickHandler}/>
+    <input type="button" value='+' className='button2' onClick={clickHandler}/>
     <input type="button" value='6' className='button' onClick={clickHandler}/>
     <input type="button" value='5' className='button' onClick={clickHandler}/>
     <input type="button" value='4' className='button' onClick={clickHandler}/>
+    <input type="button" value='-' className='button2' onClick={clickHandler}/>
     <input type="button" value='3' className='button' onClick={clickHandler}/>
     <input type="button" value='2' className='button' onClick={clickHandler}/>
     <input type="button" value='1' className='button' onClick={clickHandler}/>
+    <input type="button" value='x' className='button2' onClick={clickHandler}/>
     <input type="button" value='0' className='button' onClick={clickHandler}/>
-    <input type="button" value='.' className='button' onClick={clickHandler}/>
-    <input type="button" value='+' className='button' onClick={clickHandler}/>
-    <input type="button" value='-' className='button' onClick={clickHandler}/>
-    <input type="button" value='*' className='button' onClick={clickHandler}/>
-    <input type="button" value='/' className='button' onClick={clickHandler}/>
-    <input type="button" value='%' className='button' onClick={clickHandler}/>
+    <input type="button" value='.' className='button2' onClick={clickHandler}/>
+    <input type="button" value='/' className='button2' onClick={clickHandler}/>
+    <input type="button" value='%' className='button2' onClick={clickHandler}/>
     <input type="button" value='Clear' onClick={clearDisplay} className='button button1'/>
     <input type="button" value='=' onClick={calculate} className='button button1'/>
+    </div>   
 </div>
+
 
 
 )
--- React/calculator/src/main.jsx
+++ React/calculator/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+//import './index.css'
 import { Calculator } from './Calculator/calculator.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
